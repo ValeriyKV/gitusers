a74ced8b2fada2d70d3ccb15fbb59c179901fb6b
Korol Valerii <v.korol1998@gmail.com>
2023-12-13 17:14:56 +0200

    add parcelize

--- app/src/main/java/com/example/gitusers/data/User.kt
+++ app/src/main/java/com/example/gitusers/data/User.kt
@@ -3,7 +3,9 @@ package com.example.gitApp.data
 import android.os.Parcel
 import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class User(
     @SerializedName("login")                var login : String? = null,
     @SerializedName("id")                   var id : Int? = null,
@@ -35,87 +37,7 @@ data class User(
     @SerializedName("updated_at")           var updated_at: String? = null,
                                             var follow : Boolean = false
 ) : Parcelable{
-    constructor(parcel: Parcel) : this(
-        parcel.readString(),
-        parcel.readValue(Int::class.java.classLoader) as? Int,
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readValue(Boolean::class.java.classLoader) as? Boolean,
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readValue(Int::class.java.classLoader) as? Int,
-        parcel.readValue(Int::class.java.classLoader) as? Int,
-        parcel.readValue(Int::class.java.classLoader) as? Int,
-        parcel.readValue(Int::class.java.classLoader) as? Int,
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readBoolean()
-    ) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(login)
-        parcel.writeValue(id)
-        parcel.writeString(nodeId)
-        parcel.writeString(avatarUrl)
-        parcel.writeString(gravatarId)
-        parcel.writeString(url)
-        parcel.writeString(htmlUrl)
-        parcel.writeString(followersUrl)
-        parcel.writeString(followingUrl)
-        parcel.writeString(gistsUrl)
-        parcel.writeString(starredUrl)
-        parcel.writeString(receivedEventsUrl)
-        parcel.writeString(type)
-        parcel.writeValue(siteAdmin)
-        parcel.writeString(name)
-        parcel.writeString(company)
-        parcel.writeString(blog)
-        parcel.writeString(location)
-        parcel.writeString(email)
-        parcel.writeString(hireable)
-        parcel.writeString(bio)
-        parcel.writeString(twitterUsername)
-        parcel.writeValue(publicRepos)
-        parcel.writeValue(publicGists)
-        parcel.writeValue(followers)
-        parcel.writeValue(following)
-        parcel.writeString(created_at)
-        parcel.writeString(updated_at)
-        parcel.writeBoolean(follow)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
     fun isEmpty(): Boolean {
         return url == null
     }
-
-    companion object CREATOR : Parcelable.Creator<User> {
-        override fun createFromParcel(parcel: Parcel): User {
-            return User(parcel)
-        }
-
-        override fun newArray(size: Int): Array<User?> {
-            return arrayOfNulls(size)
-        }
-    }
-
 }
